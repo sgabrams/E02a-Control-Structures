e92699a00fd6a060862e32c81cd8e3576390fdb0
sgabrams <59935929+sgabrams@users.noreply.github.com>
2020-01-27 19:44:14 -0500

    Update main10.py

--- main10.py
+++ main10.py
@@ -4,30 +4,30 @@ import sys, random
 assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
 
-print('Greetings!')
-colors = ['red','orange','yellow','green','blue','violet','purple']
-play_again = ''
+print('Greetings!')# gives grreting to the player
+colors = ['red','orange','yellow','green','blue','violet','purple']# provides all possible answers
+play_again = ''# gives player option to play again
 best_count = sys.maxsize            # the biggest number
 
-while (play_again != 'n' and play_again != 'no'):
-    match_color = random.choice(colors)
-    count = 0
-    color = ''
+while (play_again != 'n' and play_again != 'no'):# player not electing to play again by typing no or n
+    match_color = random.choice(colors)# out of the given colors the right answer is random
+    count = 0 #number of tries is 0 zero
+    color = ''# players guessn which color is correct
     while (color != match_color):
         color = input("\nWhat is my favorite color? ")  #\n is a special code that adds a new line
-        color = color.lower().strip()
-        count += 1
-        if (color == match_color):
-            print('Correct!')
-        else:
-            print('Sorry, try again. You have guessed {guesses} times.'.format(guesses=count))
+        color = color.lower().strip()# makes sure that answers are not incorrect if typed in with or  without cpatiatls and spaces
+        count += 1# keeps tracks of the player's number of guessing
+        if (color == match_color):# describes the instance in which the player is correct
+            print('Correct!')# giving the result if the player is correct
+        else:# describing if the player guessing incorrectly
+            print('Sorry, try again. You have guessed {guesses} times.'.format(guesses=count))# giving the result ifn the player is wrong
     
-    print('\nYou guessed it in {} tries!'.format(count))
+    print('\nYou guessed it in {} tries!'.format(count))# when the game is concluded it gives the amount of guesses that were played
 
-    if (count < best_count):
-        print('This was your best guess so far!')
-        best_count = count
+    if (count < best_count):# compares the amount of guesses to other games played 
+        print('This was your best guess so far!')# lets the player no that they got it less tries than before
+        best_count = count# allows the program to remember that in this instance that its the best try
 
-    play_again = input("\nWould you like to play again (yes or no)? ").lower().strip()
+    play_again = input("\nWould you like to play again (yes or no)? ").lower().strip()# gives player the option to play again
 
-print('Thanks for playing!')
+print('Thanks for playing!')# last statement before the game ends
